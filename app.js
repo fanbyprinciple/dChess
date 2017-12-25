d05ff493b550e0e5e93a5f4098dcc88752ac5ad6
fanbyprinciple <fanbyprinciple@gmail.com>
2017-12-26 00:32:53 +0530

    Added backend of socket.io

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+    res.send('<h2>Hi, the server is working.<br>In the search bar go to /public/index.html to start </h2>');
+});
+
+io.on('connection', function(socket){
+    console.log('A user is connected');
+
+    socket.on('button',function(msg){
+        console.log(msg);
+    });
+
+    socket.on('disconnect', function(msg){
+        console.log('User disconnected');
+    });
+});
+
+http.listen(8080, function(){
+    console.log("connected to 8080.");
+});
